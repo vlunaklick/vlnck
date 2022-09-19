4098dee98a7b20542ddd41b5b813830f9523b981
Valentín Lunaklick <v.lunaklick@gmail.com>
2022-09-19 15:38:54 -0300

    feat: links in cards

--- components/WorksCard.tsx
+++ components/WorksCard.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import WorkTags from "./WorksTag";
+import Link from "next/link";
+import { BsGithub, BsDisplay } from "react-icons/bs";
 
 type Props = {
   title: string;
@@ -17,15 +19,31 @@ function WorksCard({ title, description, tags, url, github, image }: Props) {
 
   return (
     <article className="bg-slate-800 sm:px-[11px] sm:py-[13px] rounded flex sm:flex-row flex-col p-4 gap-[14px] justify-center">
-      <img src={image} alt={title} className="sm:max-h-36 rounded aspect-video" />
+      <img
+        src={image}
+        alt={title}
+        className="sm:max-h-36 rounded aspect-video"
+      />
       <div className="flex flex-col justify-between gap-3 sm:gap-[13px]">
         <div className="flex flex-col gap-[2px] ">
-          <p className="text-slate-100 text-sm font-bold">{title}</p>
-          <p className="text-slate-400 text-xs leading-5 sm:leading-4">{description}</p>
+          <div className="flex gap-2 items-center">
+            <p className="text-slate-100 text-sm font-bold">{title}</p>
+            <Link href={github} passHref>
+              <a target="_blank">
+                <BsGithub className="h-5" />
+              </a>
+            </Link>
+            <Link href={url} passHref>
+              <a target="_blank">
+                <BsDisplay className="h-5" />
+              </a>
+            </Link>
+          </div>
+          <p className="text-slate-400 text-xs leading-5 sm:leading-4">
+            {description}
+          </p>
         </div>
-        <div className="flex flex-row gap-2 flex-wrap">
-          {tagsCards}
-        </div>
+        <div className="flex flex-row gap-2 flex-wrap">{tagsCards}</div>
       </div>
     </article>
   );
