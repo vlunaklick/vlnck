cba70206a896397fcf99b8859217540034491751
Valentín Lunaklick <v.lunaklick@gmail.com>
2022-09-19 12:35:02 -0300

    feat: nav links

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,32 +1,20 @@
 import React from "react";
-import Link from "next/link";
+import NavLinks from "./NavLinks";
 
 type Props = {};
 
 function Nav({}: Props) {
   return (
     <nav className="flex justify-around items-center">
-      <Link href="/">
-        <a className="text-sm text-center transition duration-150 border-b-2 border-transparent hover:border-blue-500">
-          Home
-        </a>
-      </Link>
+      <NavLinks href={"/"} name={"Home"} />
 
-      <Link href="/exp">
-        <a className="text-sm text-center transition duration-150 border-b-2 border-transparent hover:border-blue-500">
-          Exp
-        </a>
-      </Link>
+      <NavLinks href={"/exp"} name={"Exp"} />
 
       <img src="/icon-me.png" alt="My emoji" className="rounded-full w-10" />
 
       <a className="text-sm text-slate-400 text-center">Blog</a>
 
-      <Link href="/projects">
-        <a className="text-sm text-center transition duration-150 border-b-2 border-transparent hover:border-blue-500">
-          Works
-        </a>
-      </Link>
+      <NavLinks href={"/projects"} name={"Works"} />
     </nav>
   );
 }
