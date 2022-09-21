de6ae94ddf1557c0c04a8875160677872bf9fdab
Valentín Lunaklick <v.lunaklick@gmail.com>
2022-09-21 19:09:59 -0300

    feat: analytics

--- components/Layouts/DivLayout.tsx
+++ components/Layouts/DivLayout.tsx
@@ -13,6 +13,12 @@ function DivLayout({ children }: Props) {
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/logo192.png" />
         <meta name="theme-color" content="#000000" />
+        <script
+          async
+          defer
+          data-website-id="d1111e7f-4e0d-4279-9d7f-7db04bc1c7b9"
+          src="https://analytics-umami-production-1d9d.up.railway.app/umami.js"
+        ></script>
       </Head>
 
       {children}
