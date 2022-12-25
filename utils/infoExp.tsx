d73946c57a9621ea5b7611b8d7c2d4d19bf9a3d4
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2022-12-25 01:01:07 -0300

    Update info

--- utils/infoExp.tsx
+++ utils/infoExp.tsx
@@ -1,35 +1,35 @@
 export const EXPERIENCE = [
-    {
-		start: 2022,
-		end: 'Current',
-		title: 'Junior Fullstack Developer',
-		place: 'CryptoMate',
-		URL: 'https://cryptomate.me/',
-		text: 'I joined the team working with the implementation of features for APIs made with Node.js. As time went on, I was assigned to develop them and create services in Java with Spring Boot.',
-	},
+  {
+    start: 2022,
+    end: 'Current',
+    title: 'Junior Fullstack Developer',
+    place: 'CryptoMate',
+    URL: 'https://cryptomate.me/',
+    text: 'I have been responsible for developing and maintaining APIs using Node.js, creating Java services using the Spring Boot library, and maintaining and updating the company website. My work has included implementing new features for the APIs, adding new sections and features to the website to make it modern, fast, and user-friendly, and working to improve the overall user experience.',
+  },
 ]
 
 export const EDUCATION = [
-	{
-		start: 2021,
-		end: 'Current',
-		title: 'Bachelor in Information Systems',
-		place: 'Universidad del Salvador',
-		URL: 'http://www.usal.edu.ar/institucional',
-		text: 'Currently, I am taking part of subjects like "Data structure and Algorithms", "Programming II", "Information systems" and I have programmed in languages such as Python, C and Golang.',
-	},
-	{
-		start: 2021,
-		title: 'JavaScript Algorithms and Data Structures',
-		place: 'freeCodeCamp',
-		URL: 'https://www.freecodecamp.org/',
-		text: 'A certification of Algorithms and Data Structures in Javascript that represents approximately 300 hours of coursework.',
-	},
-	{
-		start: 2021,
-		title: 'Responsive Web Design',
-		place: 'freeCodeCamp',
-		URL: 'https://www.freecodecamp.org/',
-		text: 'An HTML and CSS certification that also represents approximately 300 hours of coursework.',
-	},
+  {
+    start: 2021,
+    end: 'Current',
+    title: 'Bachelor in Information Systems',
+    place: 'Universidad del Salvador',
+    URL: 'http://www.usal.edu.ar/institucional',
+    text: 'Currently, I am taking part of subjects like "Data structure and Algorithms", "Programming II", "Information systems" and I have programmed in languages such as Python, C and Golang.',
+  },
+  {
+    start: 2021,
+    title: 'JavaScript Algorithms and Data Structures',
+    place: 'freeCodeCamp',
+    URL: 'https://www.freecodecamp.org/',
+    text: 'A certification of Algorithms and Data Structures in Javascript that represents approximately 300 hours of coursework.',
+  },
+  {
+    start: 2021,
+    title: 'Responsive Web Design',
+    place: 'freeCodeCamp',
+    URL: 'https://www.freecodecamp.org/',
+    text: 'An HTML and CSS certification that also represents approximately 300 hours of coursework.',
+  },
 ]
--- utils/infoWorks.tsx
+++ utils/infoWorks.tsx
@@ -1,29 +1,29 @@
 export const WORKS = [
   {
-    title: "Live in chat",
+    title: 'Live in chat',
     content:
-      "'Live in chat' is a clone of WhatsApp made with Next.js in the frontend and Express in the backend. Also, I implemented socket.io so I can make the connection client-server and the message display in time.",
-    src: "/project-liveinchat.png",
-    techs: ["Next.js", "socket.io", "Express", "PostgreSQL"],
-    liveview: "https://live-in-chat.vercel.app/",
-    code: "https://github.com/vlunaklick/live_in_chat",
+      'I developed a real-time chat application using Next.js and Express in the frontend and backend, respectively. In addition, I set up a separate server to host the socket.io server, which enables the client and server to communicate in real-time. The chat allows users to send and receive messages in real-time, providing a seamless and interactive communication experience.',
+    src: '/project-liveinchat.png',
+    techs: ['Next.js', 'socket.io', 'Express', 'PostgreSQL'],
+    liveview: 'https://live-in-chat.vercel.app/',
+    code: 'https://github.com/vlunaklick/live_in_chat',
   },
   {
-    title: "Frival E-Commerce",
+    title: 'Moofy',
     content:
-      "This is a clone of an e-commerce made with Next.js with the goal to improve my skills in Next.js and React and improve my UI/UX skills. I used the Next.js framework to make the project and I used the Context API to manage the state of the application.",
-    src: "/project-frival.png",
-    techs: ["Next.js", "React", "Context API", "Tailwind CSS"],
-    liveview: "https://frival-commerce.vercel.app/",
-    code: "https://github.com/vlunaklick/frival-commerce",
+      'This is an e-commerce plataform for a fictional company called Moofy. The main goal of this project is to improve my UI/UX skills and to keep on with TypeScript. Also, I wanted to apply framer motion so I could learn more about animations.',
+    src: '/project-moofy.png',
+    techs: ['React', 'framer-motion', 'Tailwind CSS'],
+    liveview: 'https://moofy-commerce.vercel.app/',
+    code: 'https://github.com/vlunaklick/moofy-commerce',
   },
   {
-    title: "Emoji Catcher & Emoji Picker",
+    title: 'Emoji Catcher & Picker',
     content:
-      "'Emoji catcher' is a recreation of a CDN developed in PHP but in Node.js. Also, it has a frontend app where you can test the functionallity.",
-    src: "/project-cdn.png",
-    techs: ["Node.js", "Express", "Next.js"],
-    liveview: "https://emoji-picker-cdn.vercel.app/",
-    code: "https://github.com/vlunaklick/cdn_emojis",
+      "In this project, I have re-implemented a CDN for emojis originally created by Ben Borgers using JavaScript and Node.js. The CDN is powered by Express and 'express-expenditious', which allows for efficient delivery of emoji files. In addition, I have developed an emoji picker feature that allows users to easily search and select from a variety of different emoji styles. This emoji picker demonstrates the functionality of the CDN API and provides a user-friendly interface for selecting and inserting emojis into text.",
+    src: '/project-cdn.png',
+    techs: ['Node.js', 'Express', 'Next.js'],
+    liveview: 'https://emoji-picker-cdn.vercel.app/',
+    code: 'https://github.com/vlunaklick/cdn_emojis',
   },
-];
+]
