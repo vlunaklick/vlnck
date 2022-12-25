47d87ebb7ec318c28f1ef375efc034eecd80c117
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2022-12-25 12:11:17 -0300

    fix nav unclickable

--- components/ThemeButton.tsx
+++ components/ThemeButton.tsx
@@ -1,11 +1,12 @@
-import { useThemes } from '../hooks/useThemes'
 import { BsSun, BsMoon } from 'react-icons/bs'
+
+import { useThemes } from '../hooks/useThemes'
 
 const ThemeButton = () => {
   const { theme, toggleTheme } = useThemes()
 
   return (
-    <div className="absolute right-4 bottom-4 w-8 top-0">
+    <div className="absolute right-4 bottom-4 w-8">
       <button
         className={
           'fixed border text-xs bottom-4 ml-8 dark:border-slate-500 p-2 dark:bg-slate-900 bg-slate-50 rounded text-slate-800 dark:text-slate-200'
