4015ddd6c4afa31cb85d95733de84c68403655e5
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2022-12-29 16:07:40 -0300

    Add blank links

--- src/components/markdowns/AnchorMd.tsx
+++ src/components/markdowns/AnchorMd.tsx
@@ -4,10 +4,16 @@ interface Props {
 }
 
 const AnchorMd = (props: Props) => {
+  const first =
+    props.children[0].charAt(0).toUpperCase() + props.children[0].slice(1)
+
   return (
     <a
       href={props.href}
       className={'hover:text-blue-500 transition-colors font-medium underline'}
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label={first}
     >
       {props.children[0]}
     </a>
