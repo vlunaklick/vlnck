db3009f05bac87466e8235cdb5a28ebd4243b48c
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2023-01-27 00:48:14 -0300

    style better og

--- src/pages/api/og.js
+++ src/pages/api/og.js
@@ -5,8 +5,18 @@ export const config = {
   runtime: 'experimental-edge',
 }
 
-export default function handler(req) {
+const interFontB = fetch(
+  'https://firebasestorage.googleapis.com/v0/b/concers-30991.appspot.com/o/fonts%2FInter-Bold.ttf?alt=media&token=7baf5dd5-c30e-412d-8ac6-8c06577961c3'
+).then(res => res.arrayBuffer())
+
+const interFontR = fetch(
+  'https://firebasestorage.googleapis.com/v0/b/concers-30991.appspot.com/o/fonts%2FInter-Regular.ttf?alt=media&token=fb8b0c26-5e5d-4311-a93a-45926f580b5f'
+).then(res => res.arrayBuffer())
+
+export default async function handler(req) {
   try {
+    const [interB, interR] = await Promise.all([interFontB, interFontR])
+
     const { searchParams } = new URL(req.url)
 
     const title = searchParams.has('title')
@@ -16,11 +26,11 @@ export default function handler(req) {
 
     return new ImageResponse(
       (
-        <div tw="h-full w-full flex items-start justify-start border border-blue-500 border-[12px] bg-gray-50">
+        <div tw="h-full w-full flex items-start justify-start border border-slate-700 border-[12px] bg-gray-50">
           <div tw="flex items-start justify-start h-full">
             <div tw="flex flex-col justify-between w-full h-full">
               <h1 tw="text-[80px] p-20 font-black text-left">{title}</h1>
-              <div tw="text-2xl pb-10 px-20 font-bold mb-0">{website}</div>
+              <div tw="text-2xl pb-10 px-20 mb-0">{website}</div>
             </div>
           </div>
         </div>
@@ -28,6 +38,20 @@ export default function handler(req) {
       {
         width: 1200,
         height: 627,
+        fonts: [
+          {
+            name: 'Inter',
+            data: interR,
+            style: 'normal',
+            weight: 400,
+          },
+          {
+            name: 'Inter',
+            data: interB,
+            style: 'normal',
+            weight: 700,
+          },
+        ],
       }
     )
   } catch (e) {
