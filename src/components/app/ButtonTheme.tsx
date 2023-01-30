eaefbb3d1678440c151d6308e68da9e1cbbf0490
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2023-01-30 19:17:04 -0300

    add useMounted

--- src/components/app/ButtonTheme.tsx
+++ src/components/app/ButtonTheme.tsx
@@ -1,17 +1,12 @@
-import { useState, useEffect } from 'react'
+import { useMounted } from 'src/hooks/useMounted'
+import { useThemes } from 'src/hooks/useThemes'
+
 import Sun from '../icons/Sun'
 import Moon from '../icons/Moon'
 
-
-import { useThemes } from '../../hooks/useThemes'
-
 const ButtonTheme = () => {
   const { theme, toggleTheme } = useThemes()
-  const [mounted, setMounted] = useState(false)
-
-  useEffect(() => {
-    setMounted(true)
-  }, [])
+  const { mounted } = useMounted()
 
   return (
     <button
@@ -21,30 +16,28 @@ const ButtonTheme = () => {
       }
       onClick={toggleTheme}
     >
-      {
-        mounted && (
-          <>
-            <div
-              className={
-                'absolute transition-transform duration-300' +
-                ' ' +
-                (theme === 'dark' ? 'translate-y-0' : '-translate-y-6')
-              }
-            >
-              <Moon className={'fill-slate-200'} />
-            </div>
-            <div
-              className={
-                'absolute transition-transform duration-300 ' +
-                ' ' +
-                (theme === 'light' ? 'translate-y-0' : 'translate-y-6')
-              }
-            >
-              <Sun className={'fill-slate-800'} />
-            </div>
-          </>
-        )
-      }
+      {mounted && (
+        <>
+          <div
+            className={
+              'absolute transition-transform duration-300' +
+              ' ' +
+              (theme === 'dark' ? 'translate-y-0' : '-translate-y-6')
+            }
+          >
+            <Moon className={'fill-slate-200'} />
+          </div>
+          <div
+            className={
+              'absolute transition-transform duration-300 ' +
+              ' ' +
+              (theme === 'light' ? 'translate-y-0' : 'translate-y-6')
+            }
+          >
+            <Sun className={'fill-slate-800'} />
+          </div>
+        </>
+      )}
     </button>
   )
 }
