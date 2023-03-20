078a208bac953ff934950e9073ab272676a7e7dd
Valentín Lunaklick <84046275+vlunaklick@users.noreply.github.com>
2023-03-20 15:44:38 -0300

    change markdown folder

--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -9,7 +9,9 @@ const PageLayout = ({ children }: Props) => {
   return (
     <>
       <Header />
+
       <main className="mx-auto flex max-w-2xl flex-col gap-4 px-4">{children}</main>
+
       <Footer />
     </>
   )
